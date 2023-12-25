ec02f424f941da76d37873ed18d25d7ccf1a4bea
Bhardwaj Bhaskar <bhaskar0120@gmail.com>
2023-12-25 21:03:16 +0530

    Day 3 Completed

--- Solution.py
+++ Solution.py
@@ -57,9 +57,81 @@ def day2(file):
     print(ans1)
     print(ans2)
 
+def day3(file):
+    import string
+    chars = [[i for i in j] for j in file.split("\n")]
+    n = len(chars)
+    m = len(chars[0])
+    grid = [[False]*m for i in range(n)]
+
+    for i in range(n):
+        for j in range(m):
+            if chars[i][j] != '.' and chars[i][j] not in string.digits:
+                grid[i][j] = True
+                for dx in range(-1,2):    
+                    for dy in range(-1,2):
+                        if i+dx < 0 or i+dx >= n or j+dy < 0 or j+dy >= m:
+                            continue
+                        grid[i+dx][j+dy] = True
+    ans = 0
+    num = 0
+    flag = False
+    for i in range(n):
+        for j in range(m):
+            if chars[i][j] in string.digits:
+                num = num*10+int(chars[i][j])
+                flag = flag or grid[i][j]
+            else:
+                if num > 0 and flag:
+                    ans += num
+                    num = 0
+                flag = False
+                num = 0
+    print(ans)  
+
+    grid = [[list() for j in range(m)] for i in range(n)]
+    counter = 0;
+    for i in range(n):
+        for j in range(m):
+            if chars[i][j] == '*':
+                for dx in range(-1,2):
+                    for dy in range(-1,2):
+                        if i+dx < 0 or i+dx >= n or j+dy < 0 or j+dy >= m:
+                            continue
+                        grid[i+dx][j+dy].append(counter)
+                counter += 1
+    ans = 0
+    num = 0
+    relation = []
+    gear_data = {}
+    for i in range(n):
+        for j in range(m):
+            if chars[i][j] in string.digits:
+                num = num*10+int(chars[i][j])
+                relation += grid[i][j]
+            else:
+                if num > 0 and len(relation) > 0: 
+                    for k in set(relation):
+                        if k not in gear_data:
+                            gear_data[k] = []
+                        gear_data[k].append(num)
+                relation = []
+                num = 0
+
+    for i in gear_data:
+        if len(gear_data[i]) == 2:
+            ans += gear_data[i][0]*gear_data[i][1]
+    print(ans)
+
+
+
+
+
+
 num_to_day = {
         '1': day1,
         '2': day2,
+        '3': day3,
         }
 
 if __name__ == "__main__":
